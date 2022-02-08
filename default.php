d4bfb453d8b89d5b6a621fb6f6d849a9efa0b4c7
unknown <zidrathan@gmail.com>
2022-02-07 21:52:20 -0700

    added default.php
    the main page for the game

---
+++ default.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>Start: A D-tier Adventure</title>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+    </head>
+    <body>
+        <div class="header">
+            <h1>Start: A D-tier Adventure</h1>
+        </div>
+        <?php
+            require('connect.php');
+            // include('nav.php');
+        ?>
+        <main>
+        </main>
+        <?php
+            // include('foot.html');
+        ?>
+    </body>
+</html>
