ef2d49fad2c8ece14fd0c24a48fcdbab39cefd18
unknown <zidrathan@gmail.com>
2022-02-13 16:46:10 -0700

    update styles

--- default.php
+++ default.php
@@ -4,6 +4,11 @@
     <head>
         <title>Start: A D-tier Adventure</title>
         <link rel="stylesheet" href="style.css">
+
+        <!--fonts-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"> 
     </head>
     <body>
         <div class="header">
@@ -13,6 +18,7 @@
             require('connect.php');
         ?>
         <main>
+            main cont -> the game
         </main>
         <?php
             include('foot.html');
