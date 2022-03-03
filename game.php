fd512971c363e52be51ecc4679b55e2a243766d7
Zidri <zidrathan@gmail.com>
2022-03-02 17:20:21 -0700

    fix options

--- game.php
+++ game.php
@@ -1,9 +1,21 @@
 <?php
 
+    //check for storyID
     if(isset($_POST['StoryID'])){
-        $IDar = str_split($_POST['StoryID']);
-        $_SESSION['cnt'] = intval($IDar[1]);
 
+        // echo("ID -> ".$_POST['StoryID']."<br>");
+
+        //split posted ID by chars
+        $IDar = str_split($_POST['StoryID']);
+
+        // print_r($IDar);
+        
+        //set cnt to posted ID num
+        $_SESSION['cnt'] = intval($IDar[1]);
+        //set alpha using posted ID letter
+        $_SESSION['alpha'] = $IDar[0];
+
+        //reset cnt at 3 -> production only
         if($_SESSION['cnt']<3){
             $_SESSION['cnt']++;
         }
@@ -11,19 +23,22 @@
             $_SESSION['cnt'] = 1;
         }
 
-        $_SESSION['alpha'] = $IDar[0];
+        
     }
     
-    if(isset($_POST['StoryID'])){        
-        // echo($_POST['StoryID']);
+    
+    if(isset($_POST['StoryID'])){    
+        //set prev texts so user can still view them
         if($_POST['StoryID'] != 'A1'){            
             if(isset( $_SESSION['lastcurtxt'])){
-                $_SESSION['pastcurtxt'] = $_SESSION['lastcurtxt'];
-                
+                //set text before last if exists
+                $_SESSION['pastcurtxt'] = $_SESSION['lastcurtxt'];                
             }
+            //set last text if not at beginning
             $_SESSION['lastcurtxt'] = $_SESSION['curtxt'];            
         }
         else{
+            //unset prev text if at begining
             unset($_SESSION['pastcurtxt']);
             unset($_SESSION['lastcurtxt']);
         }
@@ -40,21 +55,14 @@
     
         $r = $pdo->query($sql_getchoice);
 
-        $choice = $r->fetch();
-        
-        //pull all options not just first
 
-        // print_r($choice);
-
-        if(!empty($choice) && isset($choice)){
-            $choicetxt = $choice[0];
-            $choicepnt = $choice[2];
-
-            // for($i = 0; $i < count($choice); $i++){
-                // echo($choice[1][0]);
-            // }
-        }
-        
+            //run through all options
+            $optcnt = 0;
+            while($choice = $r->fetch()){
+                $options[$optcnt] = $choice;
+                // echo("<br>".$optcnt."->".$options[$optcnt][2]."<br>");                
+                $optcnt++;         
+            }       
     
         //get text for game
         $sql_gettxt = 'SELECT
@@ -69,44 +77,54 @@
         $r = $pdo->query($sql_gettxt);
         $story = $r->fetch();
 
+        //set game text ending bool
         $_SESSION['curtxt'] = $story[0];
         $_SESSION['end'] = $story[1];
     
     
         echo('<div class="gamedisp"><div class="gametext">');
     
-        //display game text  
+        
+        //display past text if possible
         if(isset($_SESSION['lastcurtxt'])){
             if(isset($_SESSION['pastcurtxt'])){
                 echo(">> ".$_SESSION['pastcurtxt']."<br><br>");
             }
             echo(">> ".$_SESSION['lastcurtxt']."<br><br>");
         }
+        //display current game text  
         echo(">> ".$_SESSION['curtxt']."<br><br>");
         
-        if(isset($choicepnt)){
+        //check if options exist
+        if(!empty($optcnt)){
             
         echo('
             </div>
             <div  id="gamebtn"> 
-            <form action="default.php" method="POST">
-            <table>
-                <tr>');
-                    // for(int i = 0; i < $choicenum; i++){
-                        //print multiple option buttons
-                        echo('<td>
-                        <input type="hidden" name="StoryID" value="'.$choicepnt.'">
-                        <input type="submit" class="btn" id="centerbtn" value="'.$choicetxt.'">
-                        </td>');
-                    // }                   
+            ');
 
-                echo('</tr>        
-            </table>
-            </form>
+                //display multiple buttons
+                for($i=0;$i<$optcnt;$i++){           
+                    echo(
+                        '<form action="default.php" method="POST">
+                        <table>
+                            <tr>
+                            <td>
+                        <input type="hidden" name="StoryID" class = "btn" value="'.$options[$i][2].'">
+                        <input type="submit" class="btn" value="'.$options[$i][0].'">
+                            </td>
+                            </tr>        
+                        </table>
+                        </form>
+            ');
+                }                   
+
+                echo('
             </div>  
             </div>        
         ');
         }
+        //check if it's an ending
         else if($_SESSION['end'] != 1){
             echo('
             </div>   
@@ -124,6 +142,7 @@
             </div>   
         ');
         }
+        //show restart button if ending
         else{
             echo('
             </div>
@@ -141,10 +160,9 @@
         </div>  
         </div>     
         ');
-        }
-        
-
+        }       
     }
+    //if there's no storyID -> show start button
     else{
         echo('
         </div>
